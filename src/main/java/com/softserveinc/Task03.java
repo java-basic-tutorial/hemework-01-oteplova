deab0f37a6ef88559e3bd0af4ee52ea7ef75b632
oteplov <oteplov@softserveinc.com>
2022-01-16 17:51:35 +0200

    HW#1 Task#3 (fix after comments)

--- src/main/java/com/softserveinc/Task03.java
+++ src/main/java/com/softserveinc/Task03.java
@@ -10,38 +10,28 @@ public class Task03 {
         System.out.println(" Write how many ml of water the coffee machine has:");
         System.out.print(">>>");
         int enteredWater = scanner.nextInt();
+
         System.out.println(" Write how many ml of milk the coffee machine has:");
         System.out.print(">>>");
         int enteredMilk = scanner.nextInt();
+
         System.out.println(" Write how many grams of coffee beans the coffee machine has:");
         System.out.print(">>>");
         int enteredCofeeBeans = scanner.nextInt();
+
         System.out.println(" Write how many cups of coffee you will need:");
         System.out.print(">>>");
         int enteredCups = scanner.nextInt();
-        int needWater = enteredCups * 200;
-        int needMilk = enteredCups * 50;
-        int needCoffeBeans = enteredCups * 15;
-        int extraWater;
-        int extraMilk;
-        int extraCoffeBeans;
-        int extraCup;
-        if (needWater <= enteredWater && needMilk <= enteredMilk && needCoffeBeans <= enteredCofeeBeans) {
-            if ((enteredWater - needWater) >= 200 && (enteredMilk - needMilk) >= 50 && (enteredCofeeBeans - needCoffeBeans) >= 15){
-                extraWater = (enteredWater - needWater) / 200;
-                extraMilk = (enteredMilk - needMilk) / 50;
-                extraCoffeBeans = (enteredCofeeBeans - needCoffeBeans) / 15;
-                extraCup = Math.min(Math.min(extraWater, extraMilk), extraCoffeBeans);
-                System.out.println(" Yes, I can make that amount of coffee (and even " + extraCup + " more than that)");
-            } else {
+
+        int minNumberOfCup = Math.min(Math.min(enteredWater/200,enteredMilk/50),enteredCofeeBeans/15);
+        if(enteredCups<=minNumberOfCup){
+            if(enteredCups-minNumberOfCup !=0){
+                System.out.println(" Yes, I can make that amount of coffee (and even " + (minNumberOfCup-enteredCups) + " more than that)");
+            }else {
                 System.out.println(" Yes, I can make that amount of coffee");
             }
         } else {
-            extraWater = enteredWater / 200;
-            extraMilk = enteredMilk / 50;
-            extraCoffeBeans = enteredCofeeBeans / 15;
-            extraCup = Math.min(Math.min(extraWater, extraMilk), extraCoffeBeans);
-            System.out.println(" No, I can make only " + extraCup + " cup(s) of coffee");
+            System.out.println(" No, I can make only " + minNumberOfCup + " cup(s) of coffee");
         }
     }
 }
